3c282d3e7b2cf93c38f259da73e46f0ce127d0f5
6631503044 <157001544+6631503044@users.noreply.github.com>
2024-10-16 20:23:59 +0700

    Add files via upload
    Start

---
+++ ambulance-app/src/main/java/com/pbt/ambulance_app/AmbulanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.pbt.ambulance_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulanceAppApplication.class, args);
+	}
+
+}
---
+++ ambulance-app/src/test/java/com/pbt/ambulance_app/AmbulanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbt.ambulance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
