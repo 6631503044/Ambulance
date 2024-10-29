81e8ecac1db25558d37c1f3b889a4b2d8c66a480
6631503044 <157001544+6631503044@users.noreply.github.com>
2024-10-30 02:56:58 +0700

    EmergencyController

---
+++ ambulance-app/src/main/java/com/pbt/ambulance_app/controller/EmergencyController.java
@@ -0,0 +1,51 @@
+package com.pbt.ambulance_app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pbt.ambulance_app.model.Emergency;
+import com.pbt.ambulance_app.repository.EmergencyRepository;
+import com.pbt.ambulance_app.model.EmergencyDTO;
+import com.pbt.ambulance_app.service.EmergencyService;
+import com.pbt.ambulance_app.model.SymptomEmgForEachCase;
+import com.pbt.ambulance_app.repository.SymptomEmgForEachCaseRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+/*Check list
+ * 1.get emergency(Not yet)
+ * 2.Add emergency(Not yet) **in check box we will sent it in array form
+ * format [Date]
+ * Symptom array....
+ * 
+ * 3.fliter??
+ * 
+ */
+
+@RestController
+@RequestMapping("/emergency")
+public class EmergencyController {
+
+    @Autowired
+    EmergencyRepository emergencyrepository;
+
+    @Autowired
+    SymptomEmgForEachCase  symptomemgforeachcase;
+
+    //have the logic of put data for emergency table and symptomforeachemergency (use it like use anthor class)
+    @Autowired
+    EmergencyService emergencyservice;
+
+    //get json at format EmergencyDTO  to Put it in Database by use Servicemethod(Just creat method!!)
+
+
+
+
+}
