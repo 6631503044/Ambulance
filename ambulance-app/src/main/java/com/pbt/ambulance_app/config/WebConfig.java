090ca1fa10b6cbc3b6afa0a5e4b5230ee1e2fcfb
koonnuthhh <157001558+6631503001@users.noreply.github.com>
2024-10-30 18:32:46 +0700

    Delete som unused import

--- ambulance-app/src/main/java/com/pbt/ambulance_app/config/WebConfig.java
+++ ambulance-app/src/main/java/com/pbt/ambulance_app/config/WebConfig.java
@@ -1,14 +1,14 @@
 package com.pbt.ambulance_app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("http://127.0.0.1:5500") // Your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
--- ambulance-app/src/main/java/com/pbt/ambulance_app/controller/PatientController.java
+++ ambulance-app/src/main/java/com/pbt/ambulance_app/controller/PatientController.java
@@ -3,11 +3,9 @@ package com.pbt.ambulance_app.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.print.DocFlavor.STRING;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
